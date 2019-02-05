d5b7506f264dbcf9d2c2645e70fd5c9ab1f67525
philbier <phillip.biermann@mail.de>
2019-02-05 17:50:13 +0100

    Update css - header and fooder
    - min-width for header and footer
    - media query for header (header font size dependend on screen width)
    - display: flex for header

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoicGhpbGF0bWFwYm94IiwiYSI6ImNqcnJ4NmxvYjBqMnQ0YW5vMzR6amhpcHcifQ.dO7u-ib0Vu2tu4JT4omMfw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
